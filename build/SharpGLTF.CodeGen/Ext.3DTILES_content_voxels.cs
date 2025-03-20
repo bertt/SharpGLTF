399fc9a89f12691b5243112a9f8a82aeae94f578
Bert Temme <btemme@hotmail.com>
2025-03-20 14:05:07 +0100

    add build extension code

---
+++ build/SharpGLTF.CodeGen/Ext.3DTILES_content_voxels.cs
@@ -0,0 +1,33 @@
+﻿using SharpGLTF.CodeGen;
+using SharpGLTF.SchemaReflection;
+using System.Collections.Generic;
+
+namespace SharpGLTF
+{
+    class Ext3DTilesContentVoxelsExtension : SchemaProcessor
+    {
+        public override string GetTargetProject() { return Constants.CesiumProjectDirectory; }
+
+        private static string RootSchemaUri => Constants.CustomExtensionsPath("3DTiles_content_voxels", "content.3DTILES_content_voxels.schema.json");
+
+        public override void PrepareTypes(CSharpEmitter newEmitter, SchemaType.Context ctx)
+        {
+            newEmitter.SetRuntimeName("3DTiles_content_voxels glTF extension", "Tiles3D_content_voxel", Constants.CesiumNameSpace);
+        }
+
+        public override IEnumerable<(string TargetFileName, SchemaType.Context Schema)> Process()
+        {
+            yield return ("Ext.3DTILES_content_voxels.g", ProcessRoot());
+        }
+
+        private static SchemaType.Context ProcessRoot()
+        {
+            var ctx = SchemaProcessing.LoadSchemaContext(RootSchemaUri);
+            ctx.IgnoredByCodeEmitter("glTF Property");
+            ctx.IgnoredByCodeEmitter("glTF Child of Root Property");
+
+            return ctx;
+        }
+
+    }
+}}
